690d85ea8339a33fd15fa40f75ed288e90a8b65b
mac8565 <surf205065@aol.com>
2024-10-31 15:04:50 -0400

    first commit

---
+++ mole.js
@@ -0,0 +1,86 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let GameOver = false;
+
+
+window.onload = function() {
+    setGame()
+}
+
+function setGame() {
+    //set up the grid for the game board in html
+    for (let i = 0; i < 9; i++) { //i goes to 0 to 8, stops at 9
+        //<div id="0-8"></div>
+        let tile = document.createElement("div");
+        tile.id= i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMole, 1000); // 1000 miliseconds = 1 seconds
+    setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds
+} 
+
+function getRandomTile() {
+    // math.random --> (0-1) * 9 = (0-9) --> round down  to (0-8) integers
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    if (GameOver) {
+        return;
+    }
+
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "./Mole.jpg";
+
+    let num = getRandomTile();
+    if (currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (GameOver) {
+        return;
+    }
+
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./Angry Plant.jpg";
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (GameOver) {
+        return;
+    }
+
+    if (this == currMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString(); //update the score
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        GameOver = true;
+    }
+}
+
+//Mole image design by Freepik, Angry Plant design by Freepik, Gaming image design by Lucie Liz
